bb736fdbb67ba5877394d58efd7b462a000cdc66
jinaldbhimani <102770228+jinaldbhimani@users.noreply.github.com>
2022-05-11 18:52:53 -0400

    Add files via upload

--- AssiLleaftapAlert.java
+++ AssiLleaftapAlert.java
@@ -1,5 +1,9 @@
 package week4.day2;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,16 +11,54 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class AssiLleaftapAlert {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		   WebDriverManager.chromedriver().setup();
 	       ChromeDriver driver=new ChromeDriver();
 	       driver.get("http://leaftaps.com/opentaps/control/login");
+	       driver.manage().window().maximize();
 	   	   driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
 		   driver.findElement(By.id("password")).sendKeys("crmsfa");
 		   driver.findElement(By.className("decorativeSubmit")).click();
 		   driver.findElement(By.linkText("CRM/SFA")).click();
 		   driver.findElement(By.xpath("//a[contains(text(),'Contacts')]")).click();
-		   driver.findElement(By.xpath("//a[@id='ext-gen868']")).click();
+		   driver.findElement(By.linkText("Merge Contacts")).click();
+		   driver.findElement(By.xpath("(//img[@alt='Lookup'])[1]")).click();
+		   Thread.sleep(3000);
+		   Set<String> window1 = driver.getWindowHandles();
+		   List<String>windowlist=new ArrayList<String>(window1);
+		   
+		   driver.switchTo().window(windowlist.get(1));
+		   driver.findElement(By.xpath("(//a[@class='linktext'])[1]")).click();
+		   driver.switchTo().window(windowlist.get(0));
+		
+		   
+		   driver.findElement(By.xpath("(//img[@alt='Lookup'])[2]")).click();
+		   Thread.sleep(3000);
+		   Set<String> window2 = driver.getWindowHandles();
+		   List<String>windowlist2=new ArrayList<String>(window2);
+		   driver.switchTo().window(windowlist2.get(1));
+		   driver.findElement(By.xpath("(//a[@class='linktext'])[6]")).click();
+		   driver.switchTo().window(windowlist2.get(0));
+		   driver.findElement(By.xpath("//a[@class='buttonDangerous']")).click();
+			
+			  driver.switchTo().alert().accept();
+			 
+			  String text =driver.findElement(By.xpath("//li[@class='errorMessage']")).getText();
+			  
+			  System.out.println(text);
+			
+		   
+		   
+		   
+		   
+		   
+		   
+		   
+		   							
+		   
+		   
+		   
+		   
 
 	}
 
